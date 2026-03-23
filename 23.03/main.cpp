3d60fad5e525cc2a08efdc31cbc8a9c1cbb051b5
root <root@HSPE-K3-105-11.ius.local>
2026-03-23 10:56:12 +0300

    tests mini-framework

--- 23.03/main.cpp
+++ 23.03/main.cpp
@@ -1,6 +1,23 @@
 #include "vector-top-it.hpp"
+#include <iostream>
+#include <iomanip>
+
+bool test1()
+{
+  using topit::Vector;
+  Vector< int > v;
+  return v.isEmpty();
+}
 
 int main()
 {
-  topit::Vector< int > v;
+  using test_t = bool(*)();
+  test_t tests[] = {
+    test1
+  };
+  size_t count = sizeof(tests) / sizeof(test_t);
+  std::cout << std::boolalpha;
+  for (size_t i = 0; i < count; i++) {
+    std::cout << tests[i]() << ": " << i << "\n";
+  }
 }
--- 23.03/vector-top-it.hpp
+++ 23.03/vector-top-it.hpp
@@ -1,11 +1,22 @@
 #ifndef VECTOR_TOP_IT_HPP
 #define VECTOR_TOP_IT_HPP
+#include <cstddef>
+
 
 namespace topit {
+
   template< class T > struct Vector {
+    Vector();
+    ~Vector();
+
+    bool isEmpty() const noexcept;
+    
+  private:
     T* data;
     size_t size, capacity;
   };
+  template< class T > bool operator==(const Vector< T > &lhs, const Vector< T > &rhs);
+
 }
 
 #endif
