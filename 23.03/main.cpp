4b77bcc1c3c99c75f014d2a4a253871f03de00d4
root <root@HSPE-K3-105-11.ius.local>
2026-03-23 12:53:31 +0300

    element access fail test

--- 23.03/main.cpp
+++ 23.03/main.cpp
@@ -2,11 +2,50 @@
 #include <iostream>
 #include <iomanip>
 
+using topit::Vector;
+
 bool test1()
 {
-  using topit::Vector;
   Vector< int > v;
   return v.isEmpty();
+}
+
+bool test2()
+{
+  Vector< int > v;
+  return v.getSize() == 0;
+}
+
+bool test3()
+{
+  constexpr size_t size = 3ull;
+  Vector< int > v(size);
+  return v.getSize() == size;
+}
+
+bool test4()
+{
+  constexpr size_t size = 3ull;
+  Vector< int > v(size);
+  try {
+    v.at(0);
+    return true;
+  } catch(...) {
+    return false;
+  }
+}
+
+bool test5() {
+  constexpr size_t size = 3ull;
+  Vector< int > v(size);
+  try {
+    v.at(size + 1);
+    return false;
+  } catch (const std::out_of_range &) {
+    return true;
+  } catch(...) {
+    return false;
+  }
 }
 
 int main()
@@ -14,7 +53,11 @@ int main()
   using test_t = bool(*)();
   using case_t = std::pair< test_t, const char* >;
   case_t tests[] = {
-    { test1, "Default constructed vector must be empty" }
+    { test1, "Default constructed vector must is empty" },
+    { test2, "Default constructed vector size is zero" },
+    { test3, "Vector constructed with size has non-zero size" },
+    { test4, "In-range access does not generate exceptions" },
+    { test5, "Out-of-range access generates std::out_of_range exception" }
   };
 
   size_t count = sizeof(tests) / sizeof(case_t);
--- 23.03/vector-top-it.hpp
+++ 23.03/vector-top-it.hpp
@@ -1,18 +1,27 @@
 #ifndef VECTOR_TOP_IT_HPP
 #define VECTOR_TOP_IT_HPP
 #include <cstddef>
+#include <stdexcept>
 
 
 namespace topit {
 
   template< class T > struct Vector {
     Vector();
+    explicit Vector(size_t size);
     ~Vector();
 
     Vector(const Vector< T >&) = delete;
     Vector< T >& operator=(const Vector< T >&) = delete;
 
     bool isEmpty() const noexcept;
+    size_t getSize() const noexcept;
+
+    T& operator[](size_t id) noexcept;
+    const T& operator[](size_t id) const noexcept;
+    T& at(size_t id);
+    const T& at(size_t id) const;
+    
 
   private:
     T* data_;
@@ -23,9 +32,23 @@ namespace topit {
 
 }
 
+template< class T > T& topit::Vector< T >::at(size_t id) {
+  throw std::logic_error("bad id");
+}
+
+
+template< class T > const T& topit::Vector< T >::at(size_t id) const {
+  throw std::logic_error("bad id");
+}
+
 template< class T > bool topit::Vector< T >::isEmpty() const noexcept
 {
-  return false;
+  return !size_;
+}
+
+template< class T > size_t topit::Vector< T >::getSize() const noexcept
+{
+  return size_;
 }
 
 
@@ -33,6 +56,12 @@ template< class T > topit::Vector< T >::Vector():
  data_(nullptr),
  size_(0),
  capacity_(0)
+{}
+
+template< class T > topit::Vector< T >::Vector(size_t size):
+ data_(size ? new T[size] : nullptr),
+ size_(size),
+ capacity_(size)
 {}
 
 template< class T > topit::Vector< T >::~Vector()
